39ccea9a12fa9f6b7640ccaeb8bbed7320b34349
jobsicke7 <doh292929@gmail.com>
2025-03-11 11:53:01 +0900

    add: damain setting

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['ssl.pstatic.net'],
+    domains: ['ssl.pstatic.net','credit-cdn-internal.wakcareers.com'],
     remotePatterns: [
       {
         protocol: 'https',
